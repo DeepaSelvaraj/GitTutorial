0314baf43435871f0da1a7fe7eaca0d0bea26392
Deepa <Deepa@Sweety.mshome.net>
2018-05-13 16:29:53 +0530

    initial commit

---
+++ com.artifact.cucumber/src/test/java/stepDefinitions/OlxSteps.java
@@ -0,0 +1,38 @@
+package stepDefinitions;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import runner.RunnerClass;
+
+public class OlxSteps extends RunnerClass{
+
+
+@Given("^user is entering google\\.com$")
+public void user_is_entering_google_com() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@When("^user clicks the search box$")
+public void user_clicks_the_search_box() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@And("^types the search term as \"([^\"]*)\"$")
+public void types_the_search_term_as(String arg1) throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+@Then("^the user should see the search results for Agniprasath$")
+public void the_user_should_see_the_search_results_for_Agniprasath() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException();
+}
+
+
+}
